0733bb325e79c612484bbb0c121b94b0f934ccc5
Mohan <mohankc596@gmail.com>
2023-10-13 22:50:53 -0700

    fall back text adde

--- src/lab_2/Driver.java
+++ src/lab_2/Driver.java
@@ -9,9 +9,26 @@ public class Driver {
         System.out.println("Enter the sentence: ");
         String Sentence=sentence.nextLine();
 
-        Counter counter= new WordProcessor();
-        counter.countWords(Sentence);
-        counter.getLength(Sentence);
-        counter.countLetters(Sentence);
+        Scanner text=new Scanner(System.in);
+        System.out.println("Enter the text: ");
+        String txt=sentence.nextLine();
+
+        WordProcessor wordProcessor=new WordProcessor();
+        wordProcessor.setText(txt);
+
+        //if the sentence is passed null
+        if (Sentence==null ||Sentence.trim().isEmpty())
+        {
+            wordProcessor.countWords(txt);
+            wordProcessor.getLength(txt);
+            wordProcessor.countLetters(txt);
+
+        }
+        else {
+            Counter counter = new WordProcessor();
+            counter.countWords(Sentence);
+            counter.getLength(Sentence);
+            counter.countLetters(Sentence);
+        }
     }
 }
--- src/lab_2/WordProcessor.java
+++ src/lab_2/WordProcessor.java
@@ -1,52 +1,111 @@
 package lab_2;
 
 public class WordProcessor implements Counter {
+    private String text;
 
     @Override
     public int countWords(String sentence) {
-        String[] array = sentence.split(" ");
-        int words = array.length;
-        System.out.println("The number of words in the given sentence is:" + words);
-        return words;
+        if (sentence==null)
+        {
+            String[] array = text.split(" ");
+            int words = array.length;
+            System.out.println("The number of words in the given sentence is:" + words);
+            return words;
+
+        }
+        else {
+            String[] array = sentence.split(" ");
+            int words = array.length;
+            System.out.println("The number of words in the given sentence is:" + words);
+            return words;
+        }
     }
 
     @Override
     public int countLetters(String sentence) {
-        //removing spaces
-        String withoutSpaces = sentence.replaceAll("\\s", "");
-        int letterCount=withoutSpaces.length();
+        if (sentence == null) {
+            //removing spaces
+            String withoutSpaces = text.replaceAll("\\s", "");
+            int letterCount = withoutSpaces.length();
 
-        int letters = 0;
-        boolean allLetters = true;
-        for (int i = 0; i < letterCount; i++) {
-            //checking whether character is letter or not
-            if (Character.isLetter(withoutSpaces.charAt(i))) {
-                letters++;
-            }
-            else {
-                //of not letter set allletters to false
-                allLetters = false;
-                break;
-            }
+            int letters = 0;
+            boolean allLetters = true;
+            for (int i = 0; i < letterCount; i++) {
+                //checking whether character is letter or not
+                if (Character.isLetter(withoutSpaces.charAt(i))) {
+                    letters++;
+                } else {
+                    //of not letter set allletters to false
+                    allLetters = false;
+                    break;
+                }
 
-        }
-        if (allLetters) {
-            System.out.println("All character in the sentence are letters");
+            }
+            if (allLetters) {
+                System.out.println("All character in the sentence are letters");
+            } else {
+                System.out.println("Character in the sentence are non-letters");
+
+            }
+            System.out.println("The number of the letter in the sentence is: " + letters);
+            return letters;
+
         } else {
-            System.out.println("Character in the sentence are non-letters");
+
+
+            //removing spaces
+            String withoutSpaces = sentence.replaceAll("\\s", "");
+            int letterCount = withoutSpaces.length();
+
+            int letters = 0;
+            boolean allLetters = true;
+            for (int i = 0; i < letterCount; i++) {
+                //checking whether character is letter or not
+                if (Character.isLetter(withoutSpaces.charAt(i))) {
+                    letters++;
+                } else {
+                    //of not letter set allletters to false
+                    allLetters = false;
+                    break;
+                }
+
+            }
+            if (allLetters) {
+                System.out.println("All character in the sentence are letters");
+            } else {
+                System.out.println("Character in the sentence are non-letters");
+
+            }
+            System.out.println("The number of the letter in the sentence is: " + letters);
+            return letters;
+        }
+
+    }
+    @Override
+    public int getLength(String sentence) {
+        if (sentence == null) {
+            System.out.println("The length of the sentence including space is: " + text.length());
 
         }
-        System.out.println("The number of the letter in the sentence is: " + letters);
-        return letters;
+        else {
+                System.out.println("The length of the sentence including space is: " + sentence.length());
+
+        }
+        return 0;
+
+
     }
 
 
-    @Override
-    public int getLength(String sentence) {
-//        String withoutSpaces=sentence.replaceAll("\\S","");
-//        int lettercount=withoutSpaces.length();
-        System.out.println("The length of the sentence including space is: " + sentence.length());
+    //setter
+    public void setText(String txt)
+    {
+        this.text=txt;
+    }
 
-        return 0 ;
+    //getter
+    public String getText()
+    {
+        return text;
     }
 }
